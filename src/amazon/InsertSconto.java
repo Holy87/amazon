869be6d7c85511a801c8fc1eae7a3974bf6cbf72
Claudio <Claudio@Claudio-AOA150.homenet.telecomitalia.it>
2014-06-20 22:53:35 +0200

    Sconti a Palate

---
+++ src/amazon/InsertSconto.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package amazon;
+
+/**
+ *
+ * @author Claudio
+ */
+public class InsertSconto {
+    String codice_sconto;
+    double sconto=0.0;
+    int idOrder;
+}
