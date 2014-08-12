25668b9131a9eb70d3ff888960e5a863ee4befe6
Giuseppe <Giuseppe@pc-tower>
2014-08-12 13:06:14 +0200

    fix modifica indirizzi

--- src/amazon/FinestraModificaMetodoPagamento.java
+++ src/amazon/FinestraModificaMetodoPagamento.java
@@ -73,39 +73,39 @@ public class FinestraModificaMetodoPagamento extends EditForm {
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel1.setText("Numero Carta di Credito");
         getContentPane().add(jLabel1);
-        jLabel1.setBounds(6, 39, 153, 28);
+        jLabel1.setBounds(6, 39, 116, 28);
 
         jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel2.setText("CIV");
         getContentPane().add(jLabel2);
-        jLabel2.setBounds(6, 215, 21, 16);
+        jLabel2.setBounds(6, 215, 17, 14);
 
         jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel3.setText("Data di Scadenza");
         getContentPane().add(jLabel3);
-        jLabel3.setBounds(6, 180, 107, 16);
+        jLabel3.setBounds(6, 180, 83, 14);
 
         tNumero.setToolTipText("Inserire il codice della carta di credito.");
         getContentPane().add(tNumero);
-        tNumero.setBounds(177, 39, 257, 28);
+        tNumero.setBounds(177, 39, 257, 20);
         getContentPane().add(tCiv);
-        tCiv.setBounds(177, 209, 52, 28);
+        tCiv.setBounds(177, 209, 52, 20);
 
         jLabel4.setText("mm");
         getContentPane().add(jLabel4);
-        jLabel4.setBounds(220, 180, 24, 16);
+        jLabel4.setBounds(220, 180, 16, 14);
         getContentPane().add(tMese);
-        tMese.setBounds(177, 174, 41, 28);
+        tMese.setBounds(177, 174, 41, 20);
 
         jLabel5.setText("aaaa");
         getContentPane().add(jLabel5);
-        jLabel5.setBounds(320, 180, 30, 16);
+        jLabel5.setBounds(320, 180, 30, 14);
         getContentPane().add(tAnno);
-        tAnno.setBounds(260, 174, 60, 28);
+        tAnno.setBounds(260, 174, 60, 20);
 
         jLabel6.setText("Indirizzo di Fatturazione");
         getContentPane().add(jLabel6);
-        jLabel6.setBounds(6, 10, 153, 16);
+        jLabel6.setBounds(6, 10, 115, 14);
 
         cContatto.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         cContatto.addActionListener(new java.awt.event.ActionListener() {
@@ -114,24 +114,24 @@ public class FinestraModificaMetodoPagamento extends EditForm {
             }
         });
         getContentPane().add(cContatto);
-        cContatto.setBounds(177, 6, 257, 27);
+        cContatto.setBounds(177, 6, 257, 20);
 
         jLabel7.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel7.setText("Nome Titolare Carta");
         getContentPane().add(jLabel7);
-        jLabel7.setBounds(6, 73, 126, 28);
+        jLabel7.setBounds(6, 73, 96, 28);
         getContentPane().add(tNome);
-        tNome.setBounds(177, 73, 257, 28);
+        tNome.setBounds(177, 73, 257, 20);
         getContentPane().add(tCognome);
-        tCognome.setBounds(177, 107, 257, 28);
+        tCognome.setBounds(177, 107, 257, 20);
 
         jLabel8.setText("Codice di Sicurezza a 3 cifre");
         getContentPane().add(jLabel8);
-        jLabel8.setBounds(235, 215, 177, 16);
+        jLabel8.setBounds(235, 215, 133, 14);
 
         jLabel9.setText("Tipo Carta");
         getContentPane().add(jLabel9);
-        jLabel9.setBounds(6, 145, 65, 16);
+        jLabel9.setBounds(6, 145, 50, 14);
 
         jButton1.setText("Annulla");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -140,7 +140,7 @@ public class FinestraModificaMetodoPagamento extends EditForm {
             }
         });
         getContentPane().add(jButton1);
-        jButton1.setBounds(329, 251, 81, 29);
+        jButton1.setBounds(329, 251, 81, 23);
 
         jButton2.setText("OK");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -149,16 +149,16 @@ public class FinestraModificaMetodoPagamento extends EditForm {
             }
         });
         getContentPane().add(jButton2);
-        jButton2.setBounds(256, 251, 67, 29);
+        jButton2.setBounds(256, 251, 67, 23);
 
         tCarta.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         getContentPane().add(tCarta);
-        tCarta.setBounds(177, 141, 255, 27);
+        tCarta.setBounds(177, 141, 255, 20);
 
         jLabel10.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel10.setText("Cognome Titolare Carta");
         getContentPane().add(jLabel10);
-        jLabel10.setBounds(6, 107, 149, 28);
+        jLabel10.setBounds(6, 107, 114, 28);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
